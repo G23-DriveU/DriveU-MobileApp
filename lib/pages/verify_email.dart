2ea5ead56e416133f6da046db785847cfe224e89
Jacob Gadberry <111665889+JacobGa03@users.noreply.github.com>
2025-01-09 22:12:12 -0500

    Fixed infinite loading screen w/ exisitng login

--- lib/pages/verify_email.dart
+++ lib/pages/verify_email.dart
@@ -7,8 +7,6 @@ import 'package:driveu_mobile_app/services/single_user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-// TODO: Ability to recheck the email verification status and allow user to resend the email if they want
-// Can we customize the email?
 class VerifyEmail extends StatefulWidget {
   const VerifyEmail({super.key});
 
@@ -17,46 +15,60 @@ class VerifyEmail extends StatefulWidget {
 }
 
 class _VerifyEmailState extends State<VerifyEmail> {
-  // TODO: We can use this timer to recheck the email verification status
   Timer? timer;
-  // Keep track of when the 'resend' button can be clicked
   bool canResend = false;
-  // Has the users email been verified?
   bool isEmailVerified = false;
-  // Call Firebase to send the email verification
+  bool isLoading = false;
+
   Future sendEmailVerification() async {
     await FirebaseAuth.instance.currentUser?.sendEmailVerification();
-    // User can't resend the email until the timer is done
     setState(() {
       canResend = false;
     });
   }
 
-  // Check the current verification status
   Future checkEmailVerification() async {
-    // Reload the user since the email verification status may have changed
     await FirebaseAuth.instance.currentUser?.reload();
-    // Update the flag
     setState(() {
       isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
     });
 
     if (isEmailVerified) {
       timer?.cancel();
+      _loadUser();
     }
   }
 
   void _loadUser() async {
-    print("Attempting to load a user from verify email page");
-    // TODO: Change this to add the actual fcm token from the device once we have it
-    AppUser? user = await UserApi.getUser({
-      'firebaseUid': FirebaseAuth.instance.currentUser!.uid,
-      'fcmToken': '123478a'
-    });
-    // Update the local AppUser singleton class
     setState(() {
-      SingleUser().setUser(user!);
+      isLoading = true;
     });
+
+    try {
+      AppUser? user = await UserApi.getUser({
+        'firebaseUid': FirebaseAuth.instance.currentUser!.uid,
+        'fcmToken': '123478a'
+      });
+      if (user != null) {
+        setState(() {
+          SingleUser().setUser(user);
+          isLoading = false;
+        });
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomePage()),
+        );
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+        print("Failed to load user data");
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      print("Error loading user data: $e");
+    }
   }
 
   @override
@@ -64,14 +76,11 @@ class _VerifyEmailState extends State<VerifyEmail> {
     super.initState();
 
     isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
-    // Send the email verification
     if (!isEmailVerified) {
       sendEmailVerification();
-
       timer = Timer.periodic(
           const Duration(seconds: 3), (_) => checkEmailVerification());
     } else {
-      // Grab the app user from our database
       _loadUser();
     }
   }
@@ -84,35 +93,25 @@ class _VerifyEmailState extends State<VerifyEmail> {
 
   @override
   Widget build(BuildContext context) {
-    return FirebaseAuth.instance.currentUser!.emailVerified
-        ? const HomePage()
-        : Scaffold(
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                      "Please verify your email address.\nAn email has been sent to ${FirebaseAuth.instance.currentUser?.email}"),
-                  const SizedBox(
-                    height: 20,
+    return Scaffold(
+      body: Center(
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : isEmailVerified
+                ? const HomePage()
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                          "Please verify your email address.\nAn email has been sent to ${FirebaseAuth.instance.currentUser?.email}"),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: canResend ? sendEmailVerification : null,
+                        child: const Text("Resend Email"),
+                      ),
+                    ],
                   ),
-                  ElevatedButton(
-                    onPressed: canResend ? sendEmailVerification : null,
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor:
-                          canResend ? Colors.white : Colors.black54,
-                      backgroundColor: canResend
-                          ? Theme.of(context).primaryColor
-                          : Colors.grey, // Text color
-                    ),
-                    child: const Text("Send Verification Again"),
-                  ),
-                  ElevatedButton(
-                      onPressed: AuthService().signOut,
-                      child: const Text("Sign out"))
-                ],
-              ),
-            ),
-          );
+      ),
+    );
   }
 }
