6aea5d6f1a963d92c800dab0a1dba23a2c6cac66
Jacob Gadberry <111665889+JacobGa03@users.noreply.github.com>
2025-01-14 20:21:38 -0500

    Made viewing past rides page stateful so we can display a list of those trips later

--- lib/pages/rides_page.dart
+++ lib/pages/rides_page.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 
 // Display all of the rides for a user. Either requests, or past rides
-class RidesPage extends StatelessWidget {
+class RidesPage extends StatefulWidget {
   const RidesPage({super.key});
+
+  @override
+  State<RidesPage> createState() => _RidesPageState();
+}
+
+class _RidesPageState extends State<RidesPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Load planned rides and previous rides
+  }
 
   @override
   Widget build(BuildContext context) {
