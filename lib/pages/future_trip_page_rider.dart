c7f20e83fe837c8e0944429e7ae106fe593d6187
Jacob Gadberry <111665889+JacobGa03@users.noreply.github.com>
2025-03-17 23:06:46 -0400

    Changed refresh logic for rider's future trip page

--- lib/pages/future_trip_page_rider.dart
+++ lib/pages/future_trip_page_rider.dart
@@ -11,12 +11,12 @@ class FutureTripPageRider extends StatefulWidget {
   FutureTripPageRider({super.key, required this.request});
 
   @override
-  State<FutureTripPageRider> createState() => _FutureTripPageRiderState(); // Create state for the widget
+  State<FutureTripPageRider> createState() =>
+      _FutureTripPageRiderState(); // Create state for the widget
 }
 
 // State class for FutureTripPageRider
 class _FutureTripPageRiderState extends State<FutureTripPageRider> {
-  
   // Reports to the API that the rider has been picked up by the driver
   Future<void> pickedUp() async {
     await TripApi().pickUpRider({
@@ -34,93 +34,155 @@ class _FutureTripPageRiderState extends State<FutureTripPageRider> {
 
     if (updatedTrip != null) {
       setState(() {
-        widget.request = updatedTrip.request!;
+        // Only update if the request has been accepted
+        widget.request = updatedTrip.request ?? widget.request;
       });
     }
   }
 
   @override
-  Widget build(BuildContext context) { // Build method to define the widget tree
-    return Scaffold( // Scaffold provides a page layout structure
-      persistentFooterButtons: [ // Footer buttons at the bottom of the page
-        Row( // Row layout to arrange buttons horizontally
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Space buttons evenly across the row
+  Widget build(BuildContext context) {
+    // Build method to define the widget tree
+    return Scaffold(
+      // Scaffold provides a page layout structure
+      persistentFooterButtons: [
+        // Footer buttons at the bottom of the page
+        Row(
+          // Row layout to arrange buttons horizontally
+          mainAxisAlignment: MainAxisAlignment
+              .spaceEvenly, // Space buttons evenly across the row
           children: [
-            ElevatedButton( // 'Picked Up' button
-              onPressed: () {}, // Currently empty onPressed callback for button functionality
-              child: Text("Picked Up 🚗", style: TextStyle(fontWeight: FontWeight.bold)), // Button label with bold text
+            ElevatedButton(
+              // 'Picked Up' button
+              onPressed:
+                  () {}, // Currently empty onPressed callback for button functionality
+              child: Text("Picked Up 🚗",
+                  style: TextStyle(
+                      fontWeight:
+                          FontWeight.bold)), // Button label with bold text
             ),
           ],
         )
       ],
-      body: Padding( // Add padding around the content
-        padding: const EdgeInsets.all(16.0), // 16px padding on all sides
-        child: ListView( // ListView allows content to be scrollable
-          children: [
-            Center( // Center the driver's profile picture
-              child: ClipOval( // Clip the image into a circular shape
-                child: Container( // Container to define image size
-                  width: 120, // Set width of the circle
-                  height: 120, // Set height of the circle
-                  child: ImageFrame( // Display driver's profile picture using ImageFrame widget
-                    firebaseUid: widget.request.futureTrip?.driver?.firebaseUid ?? '', // Use driver's Firebase UID or empty string if null
+      body: RefreshIndicator(
+        onRefresh: _refreshTrip,
+        child: Padding(
+          // Add padding around the content
+          padding: const EdgeInsets.all(16.0), // 16px padding on all sides
+          child: ListView(
+            // ListView allows content to be scrollable
+            children: [
+              Center(
+                // Center the driver's profile picture
+                child: ClipOval(
+                  // Clip the image into a circular shape
+                  child: SizedBox(
+                    // Container to define image size
+                    width: 120, // Set width of the circle
+                    height: 120, // Set height of the circle
+                    child: ImageFrame(
+                      // Display driver's profile picture using ImageFrame widget
+                      firebaseUid: widget
+                              .request.futureTrip?.driver?.firebaseUid ??
+                          '', // Use driver's Firebase UID or empty string if null
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 16), // Add space between profile picture and next section
+              SizedBox(
+                  height:
+                      16), // Add space between profile picture and next section
 
-            // Driver's name
-            ListTile( 
-              title: Text("🚗 Driver: \n${widget.request.futureTrip?.driver?.name ?? 'N/A'}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)), // Display driver's name or 'N/A' if null
-            ),
+              // Driver's name
+              ListTile(
+                title: Text(widget.request.futureTrip?.driver?.name ?? 'N/A',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize:
+                            14)), // Display driver's name or 'N/A' if null
+                subtitle: Text("🚗 Driver"),
+              ),
 
-            // Driver's rating
-            ListTile(
-              title: Text("⭐ Driver Rating: \n${widget.request.futureTrip?.driver?.driverRating ?? 'N/A'}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)), // Display driver's rating or 'N/A'
-            ),
+              // Driver's rating
+              ListTile(
+                title: Text(
+                    "⭐ Driver Rating: \n${widget.request.futureTrip?.driver?.driverRating ?? 'N/A'}",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14)), // Display driver's rating or 'N/A'
+              ),
 
-            // Start location of the trip
-            ListTile(
-              title: Text("📍 Start Location: \n${widget.request.futureTrip?.startLocation ?? 'N/A'}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)), // Display start location or 'N/A'
-            ),
+              // Start location of the trip
+              ListTile(
+                title: Text(
+                    "📍 Start Location: \n${widget.request.futureTrip?.startLocation ?? 'N/A'}",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14)), // Display start location or 'N/A'
+              ),
 
-            // Destination of the trip
-            ListTile(
-              title: Text("📌 Destination: \n${widget.request.futureTrip?.destination ?? 'N/A'}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)), // Display destination or 'N/A'
-            ),
+              // Destination of the trip
+              ListTile(
+                title: Text(
+                    "📌 Destination: \n${widget.request.futureTrip?.destination ?? 'N/A'}",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14)), // Display destination or 'N/A'
+              ),
 
-            // Pickup location for the rider
-            ListTile(
-              title: Text("🛣️ Pickup Location: \n${widget.request.riderLocation ?? 'N/A'}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)), // Display rider's pickup location or 'N/A'
-            ),
+              // Pickup location for the rider
+              ListTile(
+                title: Text(
+                    "🛣️ Pickup Location: \n${widget.request.riderLocation ?? 'N/A'}",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize:
+                            14)), // Display rider's pickup location or 'N/A'
+              ),
 
-            // Estimated pickup time, converting Unix timestamp to readable format
-            ListTile(
-              title: Text("⏰ Estimated Pickup Time: \n${widget.request.pickupTime != null ? DateTime.fromMillisecondsSinceEpoch(widget.request.pickupTime! * 1000).toString() : 'N/A'}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-            ),
+              // Estimated pickup time, converting Unix timestamp to readable format
+              ListTile(
+                title: Text(
+                    "⏰ Estimated Pickup Time: \n${widget.request.pickupTime != null ? DateTime.fromMillisecondsSinceEpoch(widget.request.pickupTime! * 1000).toString() : 'N/A'}",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+              ),
 
-            // Estimated dropoff time, converting Unix timestamp to readable format
-            ListTile(
-              title: Text("🕗 Estimated Dropoff Time: \n${widget.request.dropoffTime != null ? DateTime.fromMillisecondsSinceEpoch(widget.request.dropoffTime! * 1000).toString() : 'N/A'}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-            ),
+              // Estimated dropoff time, converting Unix timestamp to readable format
+              ListTile(
+                title: Text(
+                    "🕗 Estimated Dropoff Time: \n${widget.request.dropoffTime != null ? DateTime.fromMillisecondsSinceEpoch(widget.request.dropoffTime! * 1000).toString() : 'N/A'}",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+              ),
 
-            // Car information including color, make, and model
-            ListTile(
-              title: Text("🚙 Car: \n${widget.request.futureTrip?.driver?.name ?? 'N/A'} will be driving a ${widget.request.futureTrip?.driver?.carColor ?? 'N/A'} ${widget.request.futureTrip?.driver?.carMake ?? 'N/A'} ${widget.request.futureTrip?.driver?.carModel ?? 'N/A'}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-            ),
+              // Car information including color, make, and model
+              ListTile(
+                title: Text(
+                    "🚙 Car: \n${widget.request.futureTrip?.driver?.name ?? 'N/A'} will be driving a ${widget.request.futureTrip?.driver?.carColor ?? 'N/A'} ${widget.request.futureTrip?.driver?.carMake ?? 'N/A'} ${widget.request.futureTrip?.driver?.carModel ?? 'N/A'}",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+              ),
 
-            // Rider's cost for the trip
-            ListTile(
-              title: Text("💲 Cost: \n\\${widget.request.riderCost.toStringAsFixed(2)}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)), // Format cost to 2 decimal places
-            ),
+              // Rider's cost for the trip
+              ListTile(
+                title: Text(
+                    "💲 Cost: \n\\${widget.request.riderCost.toStringAsFixed(2)}",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14)), // Format cost to 2 decimal places
+              ),
 
-            // Distance of the trip in miles
-            ListTile(
-              title: Text("🗺️ Distance: \n${widget.request.distance.toStringAsFixed(2)} mi", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)), // Format distance to 2 decimal places
-            ),
-          ],
-
+              // Distance of the trip in miles
+              ListTile(
+                title: Text(
+                    "🗺️ Distance: \n${widget.request.distance.toStringAsFixed(2)} mi",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14)), // Format distance to 2 decimal places
+              ),
+            ],
+          ),
         ),
       ),
     );
